1a5e0bc9b5e92463c9ee2094cad008c3215f84a7
umaizas <urs8961@utulsa.edu>
2024-11-08 22:36:31 -0600

    fin all

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,7 +1,22 @@
 def fibonacci(n):
     
+    if n < 0:
+        return -1
+    
+    a = 0
+    b = 1
+
+    for n in range(0, n):
+        fib = a + b 
+        b = a
+        a = fib
+        
+        
+
+    
+    
     #write your code here
-    return
+    return a
 
 if __name__ == '__main__':
     start_num = int(input())
